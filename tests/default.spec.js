adada5bf20ad32a674682a3ac1b335dc642b358c
Divan Visagie <visagiedivan@gmail.com>
2016-12-11 18:38:39 +0200

    add unit tests

---
+++ tests/default.spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('default test to make sure tests are working', () => {
+  it('should always pass', () => {
+    expect(true).to.equal(true)
+  })
+})
